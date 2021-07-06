1da2c0dc5e80be2de19c831f7e568cb605359276
Jasper <well.caffeinated@gmail.com>
2021-07-06 13:09:19 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:minutelabsio/$REPO master:gh-pages
+git push -f git@github.com:minutelabsio/$REPO main:gh-pages
 
 cd -
